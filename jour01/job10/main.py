c22e3b207ce90aaa287058edec40b9e1daef06fd
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2023-11-20 15:40:43 +0100

    devoirs

--- jour01/job10/main.py
+++ jour01/job10/main.py
@@ -4,15 +4,19 @@ taux_rendement_annuel = 0.1
 gain_annuel = taux_rendement_annuel * montant_investissement
 print("Gain annuel initial pour la vente d'ensembles Nike : ", gain_annuel)
 
+montant_investissement += gain_annuel
 montant_investissement += 5000
-taux_rendement_annuel += 2
+taux_rendement_annuel = 0.12
 
-nouveau_gain_annuel = taux_rendement_annuel * montant_investissement / 100
+nouveau_gain_annuel = taux_rendement_annuel * montant_investissement
 print("Nouveau gain annuel après augmentation : ", nouveau_gain_annuel)
+montant_investissement += nouveau_gain_annuel
 
 montant_retire = 0.1 * montant_investissement
 montant_investissement -= montant_retire
-taux_rendement_annuel -= 1
+taux_rendement_annuel -= 0.01
 
-montant_final = taux_rendement_annuel * montant_investissement / 100
+new_gain_final = taux_rendement_annuel * montant_investissement
+print('Gain Final', new_gain_final)
+montant_final = (taux_rendement_annuel * montant_investissement) + montant_investissement
 print("Montant final après retrait pour la vente d'ensembles Nike :", montant_final)
