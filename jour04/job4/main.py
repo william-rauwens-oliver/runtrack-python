a1b3f92be9d50189a95c42d364d7aa30e7b5a4f9
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2023-11-23 16:42:49 +0100

    add new day

--- jour04/job4/main.py
+++ jour04/job4/main.py
@@ -1,15 +1,5 @@
 def fruits():
     fruits_appel = ["pomme", "cerise", "orange", "melon"]
-    fruits_appel.insert(2, "Mangue")
-    return fruits_appel
-
-resultat = fruits()
-print(resultat)
-
-# écraser orange pour remplacer par mangue.
-
-def fruits():
-    fruits_appel = ["pomme", "cerise", "orange", "melon"]
     fruits_appel[2] = "mangue"
     return fruits_appel
 
--- jour04/job7/main.py
+++ jour04/job7/main.py
@@ -5,4 +5,4 @@ multiples_de_3 = 0
 for nombre in L:
     if nombre % 3 == 0:
         multiples_de_3 +=1
-print("Nombre contenant dans le multiples de 3 est :", multiples_de_3)
+print("Nombre contenant le multiples de 3 dans la liste est :", multiples_de_3)
